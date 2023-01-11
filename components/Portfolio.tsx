41e28a7b400db69b71ad6704535d0b2bc3edb2aa
Ren Mission <missionrenjr@gmail.com>
2023-01-11 11:01:33 +0800

    refactor pf

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -109,11 +109,11 @@ const Portfolio = () => {
               ))}
             </div>
 
-            <div className="py-2">
+            <div className="py-6">
               <Link href={'https://jott-eight.vercel.app/'} target="_blank">
-                <button className="bg-transparent font-semibold text-gray-600 px-10 mt-2 py-4 sm:py-2 sm:px-4 border border-blue-700 hover:bg-blue-700 rounded hover:text-white transition-all ease-in-out shadow-md">
-                  <div className="flex justify-between px-1 ">
-                    <span className="mr-2 mt-1 pl-1">
+                <button className="text-md text-center font-semibold py-3 px-6 text-black hover:text-white cursor-pointer hover:bg-blue-700 transition ease-in-out delay-150 border border-blue-700 rounded">
+                  <div className="flex items-center">
+                    <span className="mr-2">
                       <BiLinkExternal />
                     </span>
                     <span>View Demo</span>
@@ -125,203 +125,11 @@ const Portfolio = () => {
         </section>
       ))}
 
-      {/* <section className="grid gap-12 sm:grid-cols-3 py-16">
-        <div className="col-span-2 sm:col-span-1 hidden sm:block">
-          <Image src={jott} alt="Jott - Note Taking App" className="rounded shadow-lg" />
-        </div>
-        <div className="sm:col-span-2">
-          <h2 className="text-3xl font-semibold pb-6 text-black">
-            Jott - Note Taking App
-          </h2>
-          <p className="text-xl pb-3 leading-8 max-w-4xl text-gray-600">
-            Advanced Note taking application.
-          </p>
-          <div className="py-2">
-            <Link href="https://github.com/renmission/jott" legacyBehavior>
-              <a className="text-gray-600 hover:text-blue-700 transition-all ease-in-out" target="_blank">
-                <div className="flex flex-row">
-                  <span className="text-2xl">
-                    <BiLink />
-                  </span>
-                  <span className="text-xl"> Github Repo</span>
-                </div>
-              </a>
-            </Link>
-          </div>
-
-          <div className="grid grid-cols-2 gap-6 sm:gap-4 py-4 text-gray-600">
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> ReactJS</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> React Bootstrap</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> TypeScript</span>
-            </div>
-          </div>
-
-          <div className="py-2">
-            <Link href={"https://jott-eight.vercel.app/"} target="_blank">
-              <button className="bg-transparent font-semibold text-gray-600 px-10 py-4 sm:py-2 sm:px-4 border border-blue-700 hover:bg-blue-700 rounded hover:text-white transition-all ease-in-out shadow-md">
-                <div className="flex justify-between px-1 ">
-                  <span className="mr-2 mt-1 pl-1">
-                    <BiLinkExternal />
-                  </span>
-                  <span>View Demo</span>
-                </div>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section> */}
-
-      {/* <section className="grid gap-12 sm:grid-cols-3 py-16">
-        <div className="sm:col-span-2">
-          <h2 className="text-3xl font-semibold pb-6 text-black">
-            Medium Clone
-          </h2>
-          <p className="text-xl pb-3 leading-8 max-w-4xl text-gray-700">
-            Medium clone using updated technologies with
-            <span className="text-blue-700"> front-end </span> and
-            <span className="text-blue-700"> back-end </span>.
-          </p>
-          <div className="py-2">
-            <Link
-              href="https://github.com/renmission/medium-clone"
-              legacyBehavior
-            >
-              <a className="text-gray-600 hover:text-blue-700 transition-all ease-in-out" target="_blank">
-                <div className="flex flex-row">
-                  <span className="text-2xl">
-                    <BiLink />
-                  </span>
-                  <span className="text-xl"> Github Repo</span>
-                </div>
-              </a>
-            </Link>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4 py-4 text-gray-700">
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> ReactJS / TypeScript</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> NextJS</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> TailwindCSS</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> Sanity</span>
-            </div>
-          </div>
-
-          <div className="py-2">
-            <Link href={"https://github.com/renmission"} target="_blank">
-              <button className="bg-transparent font-semibold text-gray-600 px-10 py-4 sm:py-2 sm:px-4 border border-blue-700 hover:bg-blue-700 rounded  hover:text-white transition-all ease-in-out shadow-md">
-                <div className="flex justify-between px-1 ">
-                  <span className="mr-2 mt-1 pl-1">
-                    <BiLinkExternal />
-                  </span>
-                  <span>View Demo</span>
-                </div>
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className="col-span-3 sm:col-span-1 hidden sm:block">
-          <Image src={mediumClone} alt="Medium Clone" className="rounded shadow-lg" />
-        </div>
-      </section> */}
-
-      {/* <section className="grid gap-12 sm:grid-cols-3 py-16">
-        <div className="col-span-2 sm:col-span-1 hidden sm:block">
-          <Image src={mediumClone} alt="Jott - Note Taking App" className="rounded shadow-lg" />
-        </div>
-        <div className="sm:col-span-2">
-          <h2 className="text-3xl font-semibold pb-6 text-black">
-          Medium Clone
-          </h2>
-          <p className="text-xl pb-3 leading-8 max-w-4xl text-gray-600">
-          Medium clone using updated technologies with
-            <span className="text-blue-700"> front-end </span> and
-            <span className="text-blue-700"> back-end </span>.
-          </p>
-          <div className="py-2">
-            <Link href="https://github.com/renmission/jott" legacyBehavior>
-              <a className="text-gray-600 hover:text-blue-700 transition-all ease-in-out" target="_blank">
-                <div className="flex flex-row">
-                  <span className="text-2xl">
-                    <BiLink />
-                  </span>
-                  <span className="text-xl"> Github Repo</span>
-                </div>
-              </a>
-            </Link>
-          </div>
-
-          <div className="grid grid-cols-2 gap-6 sm:gap-4 py-4 text-gray-600">
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> ReactJS</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> React Bootstrap</span>
-            </div>
-            <div className="flex flex-row">
-              <span className="text-2xl pr-2 text-blue-700">
-                <AiFillCheckCircle />
-              </span>
-              <span className="text-xl"> TypeScript</span>
-            </div>
-          </div>
-
-          <div className="py-2">
-            <Link href={"https://jott-eight.vercel.app/"} target="_blank">
-              <button className="bg-transparent font-semibold text-gray-600 px-10 py-4 sm:py-2 sm:px-4 border border-blue-700 hover:bg-blue-700 rounded hover:text-white transition-all ease-in-out shadow-md">
-                <div className="flex justify-between px-1 ">
-                  <span className="mr-2 mt-1 pl-1">
-                    <BiLinkExternal />
-                  </span>
-                  <span>View Demo</span>
-                </div>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </section> */}
-
+    
       <div className="py-16">
         <div className="max-w-7xl md:w-1/3 mx-auto">
           <Link href={'#getIntouch'}>
-            <h2 className="text-2xl text-center font-semibold p-4 text-black hover:text-white cursor-pointer hover:bg-blue-700 transition ease-in-out delay-150 border border-blue-700 rounded-lg">
+            <h2 className="text-2xl text-center font-semibold p-4 text-black hover:text-white cursor-pointer hover:bg-blue-700 transition ease-in-out delay-150 border border-blue-700 rounded">
               More, Let&apos;s Talk
             </h2>
           </Link>
